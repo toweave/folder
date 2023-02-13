ca1328c226767214bd2bcbab184c21e7f594ea1b
toweave <toweave@163.com>
2023-02-13 22:14:15 +0800

    text

--- src/run.rs
+++ src/run.rs
@@ -1,6 +1,6 @@
 use crate::handlers::input::read_line;
 use crate::handlers::operate::{read_delete_dir, read_delete_file};
-use std::io;
+use std::io::{ Result, Error};
 use std::path::Path;
 use std::process;
 // enum operations {
@@ -8,7 +8,7 @@ use std::process;
 //   Zero,
 // }
 
-pub fn start() -> io::Result<()> {
+pub fn start() -> Result<()> {
     // println!("Running");
     // read_line();
     loop {
@@ -30,10 +30,13 @@ pub fn start() -> io::Result<()> {
         // }
         match line {
             _x2 if line == two => {
-                println!("输入删除文件夹路径+文件的名称：（例如：D:/work+test.txt");
+                println!("输入删除文件夹路径+文件的名称：（例如：D:/work test.txt");
                 let line_str = read_line();
-                let path_file_name: Vec<&str> = line_str.split("+").collect();
-                println!("path_file_name = {:#?}", path_file_name);
+                let path_file_name: Vec<&str> = line_str.split(" ").collect();
+                if path_file_name.len() != 2 {
+                    println!("输入数据不正确");
+                   return Ok(());
+                }
                 let path_name = &path_file_name[0];
                 let file_name = &path_file_name[1];
                 println!(
@@ -45,10 +48,13 @@ pub fn start() -> io::Result<()> {
                 println!("操作完成！");
             }
             _x1 if line == one => {
-                println!("输入删除文件夹路径+文件夹的名称：（例如：D:/work+node_modules）");
+                println!("输入删除文件夹路径+文件夹的名称：（例如：D:/work test");
                 let line_str = read_line();
-                let path_file_name: Vec<&str> = line_str.split("+").collect();
-                println!("path_file_name = {:#?}", path_file_name);
+                let path_file_name: Vec<&str> = line_str.split(" ").collect();
+                if path_file_name.len() != 2 {
+                    println!("输入数据不正确");
+                   return Ok(());
+                }
                 let path_name = &path_file_name[0];
                 let file_name = &path_file_name[1];
                 println!(
